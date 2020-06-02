e7d2d6ea8b2a880dc157d8438fc12427ea37a1c7
Michele <micheleperilli.98@libero.it>
2020-06-02 19:06:53 +0200

    Adapted LauncherActivity to MVP pattern

--- app/src/main/java/it/sms1920/spqs/ufit/view/LauncherActivity.java
+++ app/src/main/java/it/sms1920/spqs/ufit/view/LauncherActivity.java
@@ -13,10 +13,14 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
+import it.sms1920.spqs.ufit.contract.LauncherManagerContract;
+import it.sms1920.spqs.ufit.presenter.LauncherManager;
 
 
-public class LauncherActivity extends AppCompatActivity {
+public class LauncherActivity extends AppCompatActivity implements LauncherManagerContract.view {
+
+    LauncherManager presenter;
+    Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,108 +31,98 @@ public class LauncherActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
 
-        //ImageView img = findViewById(R.id.logo);
-        //img.getLayoutParams().height = (int) getResources().getDimension(R.dimen.toolbar_height);
+        presenter = new LauncherManager(this);
 
+        //Setting search button
         Button btnSearch = findViewById(R.id.btnSearch);
         btnSearch.setOnClickListener(new FloatingActionButton.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LauncherActivity.this, SearchActivity.class));
-                overridePendingTransition(R.anim.enter_from_right, R.anim.idle);
+                presenter.onSearchIconClick();
             }
         });
 
-
-        // Starting with Home at the launch
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
 
         // Setting bottom bar
         final BottomNavigationView bottomNav = findViewById(R.id.bottom_bar);
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                Fragment selectedFragment = null;
-                Menu menu = bottomNav.getMenu();
+
+                menu.findItem(R.id.nav_home).setIcon(R.drawable.outline_home_black_48dp);
+                menu.findItem(R.id.nav_plans).setIcon(R.drawable.outline_assignment_black_48dp);
+                menu.findItem(R.id.nav_stats).setIcon(R.drawable.outline_assessment_black_48dp);
+                menu.findItem(R.id.nav_trainer).setIcon(R.drawable.outline_supervisor_account_black_48dp);
+                menu.findItem(R.id.nav_profile).setIcon(R.drawable.outline_account_box_black_48dp);
 
                 // Choosing right fragment ( click on home -> choose FragmentHome, etc.. )
                 switch (menuItem.getItemId()) {
-
                     // Home
                     case R.id.nav_home:
-                        resetBottomIcons(menu);
-                        // Marking current item icon
-                        menu.findItem(R.id.nav_home).setIcon(R.drawable.baseline_home_black_48dp);
-                        selectedFragment = new HomeFragment();
+                        presenter.onHomeIconClick();
                         break;
-
-                    // Plans
                     case R.id.nav_plans:
-                        resetBottomIcons(menu);
-                        // Marking current item icon
-                        menu.findItem(R.id.nav_plans).setIcon(R.drawable.baseline_assignment_black_48dp);
-                        selectedFragment = new PlansFragment();
+                        presenter.onPlansIconClick();
                         break;
-
-                    // Trainer
                     case R.id.nav_trainer:
-                        resetBottomIcons(menu);
-                        // Marking current item icon
-                        menu.findItem(R.id.nav_trainer).setIcon(R.drawable.baseline_supervisor_account_black_48dp);
-                        selectedFragment = new TrainerFragment();
+                        presenter.onTrainerIconClick();
                         break;
-
-                    // Stats
                     case R.id.nav_stats:
-                        resetBottomIcons(menu);
-                        // Marking current item icon
-                        menu.findItem(R.id.nav_stats).setIcon(R.drawable.baseline_assessment_black_48dp);
-                        selectedFragment = new StatsFragment();
+                        presenter.onStatsIconClick();
                         break;
-
-                    // Profile
                     case R.id.nav_profile:
-                        resetBottomIcons(menu);
-                        menu.findItem(R.id.nav_profile).setIcon(R.drawable.baseline_account_box_black_48dp);
+                        presenter.onProfileIconClick();
                         startActivity(new Intent(LauncherActivity.this, ChooseActivity.class));
                         finish();
-                        selectedFragment = new ProfileFragment();
                         break;
                 }
-
-                // Replacing old fragment with the selected one
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
                 return true;
-
             }
         });
+    }
 
+    @Override
+    public void insertHomeFragment() {
+        // Replacing old fragment with the selected one
+        menu.findItem(R.id.nav_home).setIcon(R.drawable.baseline_home_black_48dp);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+    }
 
-
-        /*final FloatingActionButton profile = findViewById(R.id.btnProfile);
-        profile.setOnClickListener(new FloatingActionButton.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(ActivityLauncher.this, Profile.class));
-                overridePendingTransition(R.anim.enter_from_right, R.anim.exit_to_left);
-
-            }
-        });*/
+    @Override
+    public void insertPlansFragment() {
+        // Replacing old fragment with the selected one
+        menu.findItem(R.id.nav_plans).setIcon(R.drawable.baseline_assignment_black_48dp);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PlansFragment()).commit();
 
     }
 
-    /**
-     * Unmark all bottom bar icons to their outline version, usefull in marking only selected one item icon in bottomBarListener
-     *
-     * @param menu Bottom bar menu
-     */
-    private void resetBottomIcons(Menu menu) {
-        menu.findItem(R.id.nav_home).setIcon(R.drawable.outline_home_black_48dp);
-        menu.findItem(R.id.nav_plans).setIcon(R.drawable.outline_assignment_black_48dp);
-        menu.findItem(R.id.nav_stats).setIcon(R.drawable.outline_assessment_black_48dp);
-        menu.findItem(R.id.nav_trainer).setIcon(R.drawable.outline_supervisor_account_black_48dp);
-        menu.findItem(R.id.nav_profile).setIcon(R.drawable.outline_account_box_black_48dp);
+    @Override
+    public void insertTrainerFragment() {
+        // Replacing old fragment with the selected one
+        menu.findItem(R.id.nav_trainer).setIcon(R.drawable.baseline_supervisor_account_black_48dp);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new TrainerFragment()).commit();
+
     }
 
+    @Override
+    public void insertStatsFragment() {
+        // Replacing old fragment with the selected one
+        menu.findItem(R.id.nav_stats).setIcon(R.drawable.baseline_assessment_black_48dp);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new StatsFragment()).commit();
+
+    }
+
+    @Override
+    public void insertProfileFragment() {
+        // Replacing old fragment with the selected one
+        menu.findItem(R.id.nav_profile).setIcon(R.drawable.baseline_account_box_black_48dp);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+
+    }
+
+    @Override
+    public void startSearchActivity() {
+        startActivity(new Intent(LauncherActivity.this, SearchActivity.class));
+        overridePendingTransition(R.anim.enter_from_right, R.anim.idle);
+    }
 }
