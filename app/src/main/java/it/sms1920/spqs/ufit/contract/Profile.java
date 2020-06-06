d3d033a2b1105d719ac3e6454416a01c384386f5
FRANCESCO <kekkosoprano251098@gmail.com>
2020-06-06 22:28:39 +0200

    implemented signOut in ProfileFragment

---
+++ app/src/main/java/it/sms1920/spqs/ufit/contract/Profile.java
@@ -0,0 +1,11 @@
+package it.sms1920.spqs.ufit.contract;
+
+public interface Profile {
+    interface View {
+        void insertHomeFragment();
+    }
+
+    interface Presenter {
+        void onSingOut();
+    }
+}
--- app/src/main/java/it/sms1920/spqs/ufit/view/ProfileFragment.java
+++ app/src/main/java/it/sms1920/spqs/ufit/view/ProfileFragment.java
@@ -1,20 +1,53 @@
 package it.sms1920.spqs.ufit.view;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-public class ProfileFragment extends Fragment {
+import it.sms1920.spqs.ufit.contract.Profile;
+import it.sms1920.spqs.ufit.presenter.ProfilePresenter;
+
+public class ProfileFragment extends Fragment implements Profile.View {
+
+    private Profile.Presenter presenter;
+    private Button btnSignOut;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_profile, container, false);
+        Log.i("pippo","Profile");
+
+        presenter = new ProfilePresenter(ProfileFragment.this);
+
+        btnSignOut = view.findViewById(R.id.btnSignOut);
+
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("pippo","Sign Out");
+                presenter.onSingOut();
+            }
+        });
         // Using the correct layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return view;
+    }
+
+    @Override
+    public void insertHomeFragment() {
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new HomeFragment() ).commit();
     }
 }
